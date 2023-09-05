5d9e5171b2eaedbf57815ca25f753f2790fb9643
Don <donbenaryl@gmail.com>
2023-09-05 22:40:38 +0800

    From local to official

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'http://vsa-be.azie-don.com',
   tokenSecretKey: '12da3c99a9c42c33347b67e452af5e8b9bad81bc4fbfb777af9749cbc6e5399d',
   apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlbWFpbEBlbWFpbC5jb20iLCJleHAiOjE2ODMyOTQzODF9.ER2SxJ2e5GkEMCfPUOv3wHV247Vv2zWpThEiwrjf3KI'
 };
