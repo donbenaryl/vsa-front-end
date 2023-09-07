5ca73cac4d257a822434bcc34a395e8f1f57ea43
Don <donbenaryl@gmail.com>
2023-09-07 14:59:29 +0800

    call api from http to https

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://virtualstaffavenue.com/api',
+  apiUrl: 'https://virtualstaffavenue.com/api',
   tokenSecretKey: '12da3c99a9c42c33347b67e452af5e8b9bad81bc4fbfb777af9749cbc6e5399d',
   apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlbWFpbEBlbWFpbC5jb20iLCJleHAiOjE2ODMyOTQzODF9.ER2SxJ2e5GkEMCfPUOv3wHV247Vv2zWpThEiwrjf3KI'
 };
